fe0475afc54936fa7e1d0f2e3b108d09733b6920
Ymaro Blue <coding.ymaro@gmail.com>
2026-05-14 15:55:12 +0200

    logo updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -57,6 +57,16 @@ export default defineNuxtConfig({
         }
     },
 
+    app: {
+        head: {
+            link: [
+                { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+                { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+            ],
+        },
+    },
+
     ssr: false,
 
     supabase: {
@@ -80,9 +90,11 @@ export default defineNuxtConfig({
             orientation: 'portrait',
             start_url: '/',
             icons: [
+                { src: '/icon-192.png', sizes: '192x192', type: 'image/png', purpose: 'any' },
+                { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any' },
+                { src: '/icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
                 { src: '/icon-192.svg', sizes: '192x192', type: 'image/svg+xml', purpose: 'any' },
                 { src: '/icon-512.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'any' },
-                { src: '/icon-512.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'maskable' },
             ],
         },
         workbox: {
